f8147caec7cb7b225dfe32db8064a42a76a4e6ca
Lucy Kwon <ks02ks222@gmail.com>
2024-02-12 23:37:11 -0800

    Created function to seed all the data from commentData.json to the comment model

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,24 +1,40 @@
 
 const sequelize = require('../config/connection');
-const { Blog, User } = require('../models');
+const { Blog, User , Comment } = require('../models');
 
+//retreiving all the default data from the json files 
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
+const commentData = require('./comment.json');
 
+//function to seed data to the database 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
+  //seed all the data from the userData.json into the User model
   const users = await User.bulkCreate(userData, {
+    //make sure to hook before create to encrypt the password
     individualHooks: true,
     returning: true,
   });
 
+  //seed all the data from the blogData.json into the Blog model 
   for (const blog of blogData) {
     await Blog.create({
       ...blog,
+      //randomly assign a user id to the blog 
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
-  }
+  };
+
+  //seed all the data from the commentData.json into the Comment model 
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  };
+
 
   process.exit(0);
 };
