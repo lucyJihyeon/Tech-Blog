55fe0a18c029f8f1b69a31a57c0dc5898774f40b
Lucy Kwon <ks02ks222@gmail.com>
2024-02-13 00:41:09 -0800

    Added homepage handlebar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
-const { Blog } = require("../models");
+const { Blog, User } = require("../models");
 
 //an user sends a GET request
 router.get("/", async (req, res) => {
   try {
     // Get all blog posts
-    const blogtData = await Blog.findAll();
+    const blogtData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
     // Serialize data so the template can read it
     const blogposts = blogtData.map((blog) => blog.get({ plain: true }));
 
@@ -13,11 +20,11 @@ router.get("/", async (req, res) => {
     res.render("homepage", {
       blogposts,
       logged_in: req.session.logged_in,
+      pageTitle: "The Tech Blog",
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 module.exports = router;
