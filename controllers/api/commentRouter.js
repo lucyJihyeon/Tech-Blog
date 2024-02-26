7442d203e368737225099296c319abd765b47d9d
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-02-25 19:41:47 -0800

    added a get router to allow user to make a comment on a blog post

--- controllers/api/commentRouter.js
+++ controllers/api/commentRouter.js
@@ -27,6 +27,31 @@ router.get("/:id", withAuth, async (req, res) => {
   }
 });
 
+
+//GET route to render all the blog details and redirect the user to allow them make a comment 
+ router.get("/create/:id", withAuth, async (req, res) => {
+  try {
+    //retrieving datas from Blog model with matching id, as well as comment and user models
+    const blog = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+    if (!blog) {
+      res.status(404).json({ message: "Blog post not found" });
+      return;
+    }
+    //sending the data to comment handlebars
+    const blogData = blog.get({ plain: true });
+    res.render("comment", { blogData, logged_in: req.session.logged_in, pageTitle: "My blog", comment: true });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+ });
+
 //POST route with withAuth middleware
 router.post("/:id", withAuth, async (req, res) => {
   try {
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
 const refresh = document.querySelector("#title");
+const blogDetail = document.querySelectorAll(".detail-blog");
+
 
 //function to refresh the window when clicked.
 const refreshHandler = () => {
@@ -6,3 +8,21 @@ const refreshHandler = () => {
 };
 
 refresh.addEventListener("click", refreshHandler);
+
+//function to redirect users to a new url to render the blog details
+const viewBlogDetail = (e) => {
+    e.preventDefault();
+    const blogId = e.currentTarget.getAttribute("data-index");
+    console.log(blogId);
+    window.location.href = `/api/comment/create/${blogId}`;
+
+}
+
+
+
+//add an event listener to each of the blog posting 
+if (blogDetail.length > 0) {
+    blogDetail.forEach(blog => {
+        blog.addEventListener("click", viewBlogDetail);
+    });
+}
